7aa693d8288439fc87836b647dbb87f16fd150fe
SARA AMHAN <121344707+PURPLEWATER00@users.noreply.github.com>
2023-05-31 00:52:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 model = pickle.load(open('model.pkl','rb'))
 
-st.header('This is a header')
+st.header('🧠  STROKE PREDICTION MODEL')
 
 st.sidebar.write('Input features: NOTE: 0 = NO, 1 = YES')
 age = st.sidebar.slider('Age:', 1, 100, 20)
