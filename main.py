15580b5bf7b3bc5c8e066e3497d14dd7de97b28c
SARA AMHAN <121344707+PURPLEWATER00@users.noreply.github.com>
2023-05-31 00:51:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 model = pickle.load(open('model.pkl','rb'))
 
-st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
+st.header('This is a header')
 
 st.sidebar.write('Input features: NOTE: 0 = NO, 1 = YES')
 age = st.sidebar.slider('Age:', 1, 100, 20)
