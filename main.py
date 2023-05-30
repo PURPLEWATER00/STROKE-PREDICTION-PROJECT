991ee577e217d4ff7f3d2b033fc3db12956da594
SARA AMHAN <121344707+PURPLEWATER00@users.noreply.github.com>
2023-05-30 22:23:10 +0300

    app

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pickle 
+import pandas as pd
+
+
+st.set_page_config(
+	page_title = 'STROKE PREDICTION MODEL',
+	page_icone = '🧠',
+    layout = 'wide',
+    initial_slidebar_state='expanded'
+)
+
+model = pickle.load(open('model.pkl','wb'))
+
+st.title = ('STROKE PREDICTION MODEL🧠')
+
+st.sidebar('Input features: NOTE: 0 = NO, 1 = YES')
+age = st.sidebar.slider('Sepal length', 1, 100, 20)
+avg_glucose_level = st.sidebar.slider('Glucose level', 1, 1000, 250)
+bmi = st.sidebar.slider('What is your BMI?', 1, 100, 24.9)
+ever_married = st.radio("Are you married?", ('Yes', 'No'))
+gender = st.radio("What is your gender?", ('Male', 'Female'))
+work_type = st.radio("Which of the following best descibes your work type?", ('Private', 'Self-employed','Govt_job', 'children', 'Never_worked'))
+Residence_type = st.radio("What is your residence type?", ('Urban', 'Rural'))
+smoking_status = st.radio("What is your smoking status?",('formerly smoked', 'never smoked', 'smokes'))
+
+test_df = [[age,avg_glucose_level, bmi, ever_married, gender, work_type, Residence_type, smoking_status]]
+
+cat_val = test_df.select_dtypes('object')
+test_df = pd.get_dummies(test_df, columns= cat_val.columns)
+test_df=x.drop(['gender_Female', 'ever_married_No', 'smoking_status_never smoked', 'Residence_type_Rural'], axis=1)
+
+pred_prob = model.predict_proba(x_test)[:,1]
+
+st.subheader('Output')
+st.metric('Predicted probability of having a stroke = ', pred_prob, '')
+
